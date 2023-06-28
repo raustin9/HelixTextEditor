7229d5f79f40c5e3fdfb2512fe1c915ae5b31463
Reagan Alexander Austin <raustin9@hydra1.eecs.utk.edu>
2023-06-28 12:31:38 -0400

    add global state and get window size

--- src/helix.c
+++ src/helix.c
@@ -1,18 +1,33 @@
 /* INCLUDES */
+#include <asm-generic/ioctls.h>
 #include <ctype.h>
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
 /* DEFINES */
 #define CTRL_KEY(k) ((k) & 0x1f) // makes the first 3 bits of character 0 to make it ctrl-key
 
 /* DATA */
-struct termios orig_termios;
+// struct termios orig_termios;
+
+// Gobal State
+struct EditorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct EditorConfig E;
 
 /* TERMINAL */
+
+// Perform error handling actions
+// and kill process
 void
 Die(const char* s) {
   write(STDOUT_FILENO, "\x1b[2J", 4); // clears the screen
@@ -22,23 +37,29 @@ Die(const char* s) {
   exit(1);
 }
 
+// Reset the terminal attributes
+// to the original values 
+// Used when the program exits
 void
 DisableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
     Die("tcsetattr");
   }
 }
 
+// Disable canonical mode in the terminal
+// and enable 'raw' mode so that we can 
+// easily draw to the screen and read inputs
 void
 EnableRawMode() {
   struct termios raw;
 
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
     Die("tcgetattr");
   }
   atexit(DisableRawMode);
 
-  raw = orig_termios;
+  raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8); // set 8 bits per byte
@@ -51,6 +72,8 @@ EnableRawMode() {
   }
 }
 
+// Read a key from 
+// user input
 char
 EditorReadKey() {
   int nread;
@@ -65,16 +88,61 @@ EditorReadKey() {
   return c;
 }
 
+int
+GetCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  uint32_t i;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  i = 0;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
+// Get the dimensions of 
+// the terminal window
+int
+GetWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    // Move the cursor to the bottom right of screen
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+      return -1;
+    }
+    return GetCursorPosition(rows, cols);
+
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /* OUTPUT */
+
+// Draw the rows to the screen
 void
 EditorDrawRows() {
   int y;
 
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "$\r\n", 3);
   }
 }
 
+// Clear the screen and perform other
+// actions to draw to the screen
 void
 EditorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4); // clears the screen
@@ -87,6 +155,8 @@ EditorRefreshScreen() {
 
 /* INPUT */
 
+// Process the logic for which key 
+// was pressed
 void
 EditorProcessKeypress() {
   char c;
@@ -103,9 +173,20 @@ EditorProcessKeypress() {
 }
 
 /* INIT */
+
+// Initialize the editor
+void
+InitEditor() {
+  if (GetWindowSize(&E.screenrows, &E.screencols) == -1) {
+    Die("Get window size");
+  }
+}
+
+// Main function
 int
 main(int argc, char** argv) {
   EnableRawMode();
+  InitEditor();
 
   while (1) {
     EditorRefreshScreen();
